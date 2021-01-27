e15d44843d9a6549ec2c62eec27dc8ec8c6a370b
ron93 <ronmkare1@gmail.com>
2021-01-27 10:47:41 +0300

    cart_screen: Column child widget -- estimated delivery time

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:instant_delivery/data/data.dart';
 import 'package:instant_delivery/models/order.dart';
 
@@ -8,30 +10,160 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  Widget _buildCartItem(Order order) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        height: 170.0,
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Row(
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
 
 
+                Expanded(child: Container(
+                  margin: EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        order.product.name,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+
+                      SizedBox(height: 10.0,),
+                      Text(
+                        order.shop.name,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 10.0,),
+                      Container(
+                        width: 100.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(
+                              width: 0.8,
+                              color: Colors.black54,
+                            )
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                '-',
+                                style: TextStyle(
+                                  color: Theme
+                                      .of(context)
+                                      .primaryColor,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10.0,),
+                            Text(
+                              order.quantity.toString(),
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            SizedBox(width: 20.0,),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                '+',
+                                style: TextStyle(
+                                  color: Theme
+                                      .of(context)
+                                      .primaryColor,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+                )
+              ],
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: Text(
+                '\$${order.quantity * order.product.price}',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            )
+          ],
+        )
+    );
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    double totalPrice = 0 ;
-    currentUser.cart.forEach((Order order) => totalPrice +=order.product.price);
+    double totalPrice = 0;
+    currentUser.cart.forEach((Order order) =>
+    totalPrice += order.product.price);
 
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart(${currentUser.cart.length})'),
       ),
-    body: ListView.separated(
-      itemBuilder: (BuildContext context,int index){
-        if(index < currentUser.cart.length){
-          Order order = currentUser.cart[index];
-          return _buildCartItem(order);
-        }
-      },
-      separatorBuilder: null,
-      itemCount: null),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            if (index < currentUser.cart.length) {
+              Order order = currentUser.cart[index];
+              return _buildCartItem(order);
+            }
+            return Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Estimated delivery Time:',
+                    style: TextStyle(fontSize:20.0,
+                    fontWeight:FontWeight.w600,
+                  )
+                  )
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(
+              height: 1.0,
+              color: Colors.grey,
+            );
+          },
+          itemCount: currentUser.cart.length + 1),
     );
   }
-
-
+}
